3fd0c2fe33b5629f2e450596728ef78b5d3e15ea
Md Abu Nayeb Towki <94976016+nayebtowki@users.noreply.github.com>
2026-08-05 00:50:39 +0100

    completed typecasting exercise

--- src/main/java/com/amigoscode/_1_beginners/_1_thebasics/TypeCasting.java
+++ src/main/java/com/amigoscode/_1_beginners/_1_thebasics/TypeCasting.java
@@ -14,31 +14,52 @@ public class TypeCasting {
         // TODO: 1 - Widen an int to a double (implicit casting)
         // Declare an int variable with any value, then assign it to a double variable.
         // Print both variables to see the result.
+        int intiger = 257;
+        double doubleValue = intiger;
+        System.out.println(doubleValue);
 
 
         // TODO: 2 - Narrow a double to an int (explicit casting)
         // Declare a double variable (e.g., 9.78), then cast it to an int.
         // Print both variables to see what happens to the decimal part.
+        double value = 9.78;
+        int narrowValue = (int) value;
+        System.out.println(value);
+        System.out.println(narrowValue);
 
 
         // TODO: 3 - Cast an int to a char to get the character it represents
         // Hint: int value 65 corresponds to 'A' in ASCII
         // Print the resulting char.
+        int cint = 65;
+        char chint = (char) cint;
+        System.out.println(chint);
+
+
 
 
         // TODO: 4 - Cast a char to an int to get its ASCII value
         // Hint: char 'Z' has an ASCII value of 90
         // Print the resulting int.
+        char al = 'Z';
+        int intal = (int) al;
+        System.out.println(intal);
 
 
         // TODO: 5 - Convert a String "42" to an int using Integer.parseInt()
         // Declare a String variable with the value "42", then parse it to an int.
         // Print the result.
+        String num = "42";
+        int sttoNum = Integer.parseInt(num);
+        System.out.println(sttoNum);
 
 
         // TODO: 6 - Convert an int 42 to a String using String.valueOf()
         // Declare an int variable with the value 42, then convert it to a String.
         // Print the result.
+        int var = 42;
+        String strtoInt = String.valueOf(var);
+        System.out.println(strtoInt);
 
     }
 }
