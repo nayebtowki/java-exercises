9ea9020f14c28355cb67b99a42f617069e315071
Md Abu Nayeb Towki <94976016+nayebtowki@users.noreply.github.com>
2026-08-19 16:48:54 +0100

    completed methodOverloading exercises

--- src/main/java/com/amigoscode/_2_developers/_5_methods/MethodOverloading.java
+++ src/main/java/com/amigoscode/_2_developers/_5_methods/MethodOverloading.java
@@ -11,28 +11,47 @@ public class MethodOverloading {
 
     // TODO: 1 - Create a method: int add(int a, int b)
     //  Returns the sum of two integers.
+    static int add(int a, int b){
+        return a + b;
+    }
 
 
     // TODO: 2 - Create an overloaded method: int add(int a, int b, int c)
     //  Returns the sum of three integers.
+    static int add(int a,int b,int c){
+        return a+b+c;
+    }
 
 
     // TODO: 3 - Create an overloaded method: double add(double a, double b)
     //  Returns the sum of two doubles.
+    static double add(double a, double b){
+        return a+b;
+    }
 
 
     // TODO: 4 - Create a method: String format(String value)
     //  Returns the string wrapped in square brackets, e.g., "[hello]".
+    static String format(String value){
+        return "["+ value + "]";
+    }
 
 
     // TODO: 5 - Create an overloaded method: String format(int value)
     //  Returns the integer formatted with leading zeros to 5 digits.
     //  Example: format(42) returns "00042".
     //  Hint: use String.format("%05d", value)
+    static String format(int value){
+        return String.format("%05d", value);
+    }
 
 
     // TODO: 6 - Create an overloaded method: String format(String label, int value)
     //  Returns "label: value", e.g., format("Score", 95) returns "Score: 95".
+    static String format(String label, int value){
+
+        return label + ": " + value;
+    }
 
 
     public static void main(String[] args) {
@@ -40,12 +59,17 @@ public class MethodOverloading {
 
         // TODO: 7 - Call each overloaded method and print the results:
         //  - add(2, 3)
+        System.out.println(add(2,3));
         //  - add(1, 2, 3)
+        System.out.println(add(1, 2,3));
         //  - add(1.5, 2.5)
+        System.out.println(add(1.5,2.5));
         //  - format("hello")
+        System.out.println(format("Hello"));
         //  - format(42)
+        System.out.println(format(42));
         //  - format("Score", 95)
+        System.out.println(format("Score",95));
         //  Print each result with a descriptive label.
-
     }
 }
